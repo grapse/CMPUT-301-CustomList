253e79ae8d9b9128551aed63540e5685f2dfe2cb
grape <leah.ren@hotmail.com>
2021-11-01 15:22:51 -0600

    GREEN: count city test

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -61,7 +61,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int countCities(){
-        return 0;
+        return cities.size();
     }
 
     public void addCity(City city){
